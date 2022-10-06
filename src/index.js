f7eb80fb1e594b4d51b8420e0d5a669a354e2981
Loss Shirley <shirleyloss@LossdeMacBook-Pro.local>
2022-10-05 19:44:24 -0700

    Finished Demo.

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import './index.css'
+import App from './App'
+
+import './api/server'
+import store from './store'
+import { fetchTodos } from './features/todos/todosSlice'
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+)
+
+store.dispatch(fetchTodos())
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById('root')
+)
